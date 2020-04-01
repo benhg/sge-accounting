2eb85734164bf5f8d423bb4a25d716cd63309169
Ben Glick <glick@glick.cloud>
2020-04-01 11:55:29 -0700

    skip commented lines

--- sge_generate_accounting_graphs.py
+++ sge_generate_accounting_graphs.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     parser.add_argument("--input", help="Path to accounting file. Must be readable by user.", type=str, required=True)
     args = parser.parse_args()
     schema = ["qname", "hostname", "group", "owner", "job_name", "job_number", "account", "priority", "submission_time", "start_time", "end_time", "failed", "exit_status", "ru_wallclock", "project", "department", "granted_pe", "slots", "task_number", "cpu", "mem", "io", "category", "iow", "pe_taskid", "maxvmem", "arid", "ar_submission_time"]
-    input_df = pd.read_csv(args.input, sep=":", names=schema)
+    input_df = pd.read_csv(args.input, sep=":", names=schema, skiprows=4)
     eval(args.task)(input_df)
