fd90a286de2d99b44a953aa7865d241fc79854e4
Ben Glick <glick@glick.cloud>
2020-04-01 11:45:17 -0700

    add arg parser

--- sge_generate_accounting_graphs.py
+++ sge_generate_accounting_graphs.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+
+if __name__ == '__main__':
+	import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--graph", help="Graph to generate. Must be name of a function in this object.", type=str, required=True)
+    parser.add_argument("--input", help="Path to accounting file. Must be readable by user.", type=str, required=True)
+    args = parser.parse_args()
+    
