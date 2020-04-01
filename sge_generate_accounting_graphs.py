0344d9adff86e9042b477106708d279a833db2f9
Ben Glick <glick@glick.cloud>
2020-04-01 11:52:50 -0700

    add file schema

--- sge_generate_accounting_graphs.py
+++ sge_generate_accounting_graphs.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     parser.add_argument("--task", help="Task to run. Must be name of a function in this object.", type=str, required=True)
     parser.add_argument("--input", help="Path to accounting file. Must be readable by user.", type=str, required=True)
     args = parser.parse_args()
-    schema = None
+    schema = ["qname", "hostname", "group", "owner", "job_name", "job_number", "account", "priority", "submission_time", "start_time", "end_time", "failed", "exit_status", "ru_wallclock", "project", "department", "granted_pe", "slots", "task_number", "cpu", "mem", "io", "category", "iow", "pe_taskid", "maxvmem", "arid", "ar_submission_time"]
     input_df = pd.read_csv(args.input, sep=":", names=schema)
-    parser.task(input_df)
+    eval(args.task)(input_df)
