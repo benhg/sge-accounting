565e47c5cf0e0fe2baeee9f22bfd814d9d99e791
Ben Glick <glick@glick.cloud>
2020-04-01 11:48:41 -0700

    testing

--- sge_generate_accounting_graphs.py
+++ sge_generate_accounting_graphs.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
+def print_head(df):
+    print(df.head())
 
 if __name__ == '__main__':
-	import argparse
+    import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--graph", help="Graph to generate. Must be name of a function in this object.", type=str, required=True)
+    parser.add_argument("--task", help="Task to run. Must be name of a function in this object.", type=str, required=True)
     parser.add_argument("--input", help="Path to accounting file. Must be readable by user.", type=str, required=True)
     args = parser.parse_args()
-    
+    schema = None
+    input_df = pd.read_csv(args.input, sep=":", names=schema)
+    parser.task(input_df)
